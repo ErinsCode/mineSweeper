2b93a9d36f6305df533765f8b8d9618613595317
ErinsCode <74279783+ErinsCode@users.noreply.github.com>
2020-11-10 19:43:07 -0700

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,104 @@
+package minesweeperGame;
+
+/**
+ * A minesweeper cell.
+ * 
+ * @author Erin Mortensen
+ *
+ */
+public class Cell
+{
+	int value;
+	boolean isFlagged;
+	boolean isVisible;
+
+	/**
+	 * Initializes the value of the cell. If the value is 9 then the cell holds
+	 * a mine. Values 0 - 8 stand for the number of mines that the cell touches.
+	 * 
+	 * @param value Value of the cell. 9 is a mine, and 0-8 represents the number of mines it touches.
+	 */
+	public Cell(int value)
+	{
+		this.value = value;
+		this.isFlagged = false;
+		this.isVisible = false;
+	}
+
+	/**
+	 * 
+	 * @param value Value of the cell. 9 is a mine, and 0-8 represents the number of mines it touches.
+	 * @param isFlagged Flags the cell so that individual knows that a mine is underneath
+	 * @param isVisable Says if the user can see the value of the cell or not, or if it is covered.
+	 */
+	public Cell(int value, boolean isFlagged, boolean isVisable)
+	{
+		this(value);
+		this.isFlagged = isFlagged;
+		this.isVisible = isVisable;
+	}
+
+
+	/**
+	 * Gets the value of the mine.
+	 * 
+	 * @return the mines value
+	 */
+	public int getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * Updates the cell's value.
+	 * 
+	 * @param value Sets the new value of the cell. 9 makes the cell a mine, 0-8 stands for the number of other mines that this cell is touching.
+	 */
+	public void setValue(int value)
+	{
+		this.value += value;
+	}
+
+	/**
+	 * Returns if the cell has been flagged
+	 * 
+	 * @return the if cell is flagged
+	 */
+	public boolean isFlagged()
+	{
+		return isFlagged;
+	}
+
+	/**
+	 * Changes if the cell is flagged or not.
+	 * 
+	 * @param isFlagged sets if the cell is flagged.
+	 */
+	public void setFlagged(boolean isFlagged)
+	{
+		this.isFlagged = isFlagged;
+	}
+
+	/**
+	 * Returns if the cell's value is visible.
+	 * 
+	 * @return the if cell is visible.
+	 */
+	public boolean isVisable()
+	{
+		return isVisible;
+	}
+
+	/**
+	 * Sets the visibility of the cell's value.
+	 * 
+	 * @param isVisable If the visibility is true or false.
+	 */
+	public void setVisable(boolean isVisable)
+	{
+		this.isVisible = isVisable;
+	}
+
+	
+
+}
