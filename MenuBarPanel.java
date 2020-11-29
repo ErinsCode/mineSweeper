6ddbe1a1b6d893027fe2f6954ca11ae10791fc72
ErinsCode <74279783+ErinsCode@users.noreply.github.com>
2020-11-29 11:28:52 -0700

    Update - idea of how to structure saved text file

--- MenuBarPanel.java
+++ MenuBarPanel.java
@@ -9,6 +9,8 @@ import javax.swing.JButton;
 
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
@@ -30,11 +32,12 @@ public class MenuBarPanel extends JPanel {
 	private JButton newGameButton;
 	private JButton saveButton;
 	private JButton loadButton;
+	private MinesweeperWindow parentWindow;
 	
 	/**
 	 * Create the panel with 3 buttons.
 	 */
-	public MenuBarPanel() {
+	public MenuBarPanel(MinesweeperWindow parentWindow) {
 		setOpaque(false);
 		setBackground(Color.ORANGE);
 		setBounds(0,0,200,30);
@@ -52,6 +55,8 @@ public class MenuBarPanel extends JPanel {
 		
 		loadButton = createLoadButton();
 		add(loadButton);
+		
+		this.parentWindow = parentWindow;
 
 	}
 
@@ -66,6 +71,7 @@ public class MenuBarPanel extends JPanel {
 		newGameButton.setBorder(null);
 		newGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+			
 				//TO DO: Start new game
 			}
 		});
@@ -100,6 +106,8 @@ public class MenuBarPanel extends JPanel {
 		loadButton.setBorder(null);
 		loadButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				loadGame();
 				//TO DO: Load game
 			}
 		});
@@ -111,12 +119,56 @@ public class MenuBarPanel extends JPanel {
 	 */
 	public void startNewGame() {
 		
+
+	
 	}
 	
 	/**
 	 * Saves a game to a text file.
 	 */
 	public void saveGame() {
+		boolean gameOver = parentWindow.getGame().getGameBoard().getIfGameOver();
+		
+		if(gameOver)
+		{
+			//message stating that you cannot save a game that has already been won/lost
+			return;
+		}
+		
+		//to do save game
+	}
+	
+	public void loadGame()
+	{
+		int row = 2;
+		int column = 4;
+		List<Cell> cells = new ArrayList<>();
+		
+		cells.add(new Cell(1,0,0,false, false));
+		cells.add(new Cell(9,0,1,false, false));
+		cells.add(new Cell(9,0,2,true, false));
+		cells.add(new Cell(0,0,3,true, false));
+		cells.add(new Cell(1,1,0,false, false));
+		cells.add(new Cell(9,1,1,false, false));
+		cells.add(new Cell(1,1,2,false, true));
+		cells.add(new Cell(0,1,3,true, false));
+		
+		MineField game = new MineField(row, column, cells);
+		
+		MineFieldPanel gameBoard = new MineFieldPanel(parentWindow, row, column, game);
+		
+		/*
+		 * how do we want to set up the loaded game?  Do we want
+		 * a method in MinesweeperWindow that sets the MineFieldPanel, or
+		 * do we want to just have an overloaded constructor that deals with this?
+		 * 
+		parentWindow.setMineFieldPanel(gameBoard);
+		
+		
+		*/
+		
+
+		
 		
 	}
 	
